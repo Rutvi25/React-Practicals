ee2ebd3a0a6f7030f634803d46ecfe15800c8457
Rutvi25 <rutvipatel2500@gmail.com>
2022-03-06 23:45:47 +0530

    add new tasks

--- Practical-3/todo-react-app/src/Components/AddTodo/AddTodo.js
+++ Practical-3/todo-react-app/src/Components/AddTodo/AddTodo.js
@@ -1,15 +1,53 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSquareCheck, faTimesSquare} from '@fortawesome/free-solid-svg-icons';
 
 import './AddTodo.css'
 
-class AddTodo extends Component {
-  render() {
-    return (
-      <div className='add-btn-container'>
-        <button className='add-todo-button'>+</button>
-      </div>
-    )
+
+function AddTodo() {
+  const [displayForm, setDisplayForm] = useState(false)
+  const [addTaskTitle, setAddTaskTitle] = useState('')
+  const [taskItems, setTaskItems] = useState([])
+
+  const addNewTask = () => {
+    setTaskItems([ ...taskItems, addTaskTitle ]);
+    setAddTaskTitle('')
+
   }
+
+  function handleKeyPress(e) {
+    if(e.keyCode === 27) {
+      setDisplayForm(false)
+    }
+    else if (e.keyCode === 13) {
+      addNewTask()
+    }
+  }
+ 
+  let showForm;
+  if(displayForm){
+    showForm = 
+      <div className='add-todo-form'>
+          <input type="text" onKeyDown={handleKeyPress} value={addTaskTitle} onChange={(e) => setAddTaskTitle(e.target.value)}/>
+          <span>
+          <FontAwesomeIcon icon={faSquareCheck} onClick={addNewTask}/>
+          </span>
+          <span>
+          <FontAwesomeIcon icon={faTimesSquare} onClick={() => setDisplayForm(false)}/>
+          </span>
+          <div>{taskItems}</div>
+      </div>
+  }
+  else {
+    showForm = 
+      <div className='add-btn-container'>
+        <button className='add-todo-button'  onClick={() => setDisplayForm(true)}>+</button>
+      </div>
+  }
+  return (
+    <div>{showForm}</div>
+  )
 }
 
 export default AddTodo
