8cd5485e7d3671d9450d31364033012aff935c3f
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-03-21 18:21:47 +0530

    removed log

--- Practical-5/user-list-react-app/src/reducer/screenReducer.js
+++ Practical-5/user-list-react-app/src/reducer/screenReducer.js
@@ -2,7 +2,6 @@ const screenReducer = (state = {}, action) => {
   switch (action.type) {
     case "SCREEN_RESIZE":
       const { screenWidth } = action;
-      console.log(screenWidth)
       return { ...state, screenWidth };
     default:
       return state;
