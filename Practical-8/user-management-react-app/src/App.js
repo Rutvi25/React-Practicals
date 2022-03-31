96877e7c7f2165739c65481d88b0040b64905fee
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-03-31 09:35:57 +0530

    added the routes

--- Practical-8/user-management-react-app/src/App.js
+++ Practical-8/user-management-react-app/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom'
+
 import './App.css';
+import SignUp from './components/SignUp';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      User Management React App
-    </div>
+    <>
+      <Routes>
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
